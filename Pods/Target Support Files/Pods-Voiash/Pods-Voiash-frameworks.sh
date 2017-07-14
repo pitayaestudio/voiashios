c35cbe91eedd387f47c118774ecdcfe260bd7fde
Brenda Saavedra <brenda@muapp.me>
2017-07-14 13:01:33 -0500

    changes

--- Pods/Target Support Files/Pods-Voiash/Pods-Voiash-frameworks.sh
+++ Pods/Target Support Files/Pods-Voiash/Pods-Voiash-frameworks.sh
@@ -105,6 +105,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Spring/Spring.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TextFieldEffects/TextFieldEffects.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
@@ -123,6 +124,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Spring/Spring.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TextFieldEffects/TextFieldEffects.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/lottie-ios/Lottie.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- Voiash/Controller/VOInitialVC.swift
+++ Voiash/Controller/VOInitialVC.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import Lottie
 
 class VOInitialVC: UIViewController {
 
     @IBOutlet weak var vFlightClouds:FlightCloudsView!
+    @IBOutlet weak var vAnimation:UIView!
+    
+    var animationView:LOTAnimationView!
     var isActive = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        animationView = LOTAnimationView(name: "streetby_test_loading")
+        animationView.loopAnimation = true
+        animationView.contentMode = .scaleAspectFill
+        self.vAnimation.addSubview(animationView)
+        animationView.play()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,14 +31,22 @@ class VOInitialVC: UIViewController {
         self.navigationController?.isNavigationBarHidden = true
         self.navigationItem.title = " "
         self.isActive = true
-        vFlightClouds.addFlightCloudsAllScreenAnimation()
+        //vFlightClouds.addFlightCloudsAllScreenAnimation()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        vFlightClouds.removeAllAnimations()
+        //vFlightClouds.removeAllAnimations()
         self.navigationController?.isNavigationBarHidden = true
         self.isActive = false
         super.viewWillDisappear(animated)
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //let lottieRect = CGRect(x: 0, y: 60, width: self.view.bounds.size.width, height: self.view.bounds.size.height - 60)
+        let lottieRect = CGRect(x: 0, y: 60, width: self.vAnimation.bounds.size.width, height: self.vAnimation.bounds.size.height)
+        self.animationView.frame = lottieRect
     }
     
     // MARK: - IBAction
